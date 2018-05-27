cd53cbc227e2f412bf70015b1b13c9b05942420b
vincent178 <vh7157@gmail.com>
2018-05-27 20:48:05 +0800

    update

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const Fastify = require('fastify')
+const config = require('./config.json');
 
+if (process.env.NODE_ENV !== 'production'){
+  require('longjohn');
+}
 
 const proxy = Fastify({
   logger: true
 })
 
 proxy.register(require('fastify-reply-from'), {
-  base: 'https://ruby-china.org/'
+  base: 'https://ruby-china.org'
 });
 
 function rewriteHeaders(headers) {
@@ -14,10 +18,15 @@ function rewriteHeaders(headers) {
   return headers;
 }
 
-proxy.post('/auth/token', (request, reply) => {
-  reply.from('/auth/token', {rewriteHeaders});
+proxy.post('/oauth/token', (request, reply) => {
+  reply.from('/oauth/token', {rewriteHeaders, body: {
+    ...request.body,
+    client_id: config.RubyChina.client_id,
+    client_secret: config.RubyChina.client_secret
+  }});
 });
 
+proxy.all('/*', (request, reply) => reply.from(request.req.url));
 
 proxy.listen(4000, (err) => {
   if (err) throw err;
